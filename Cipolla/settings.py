a0c56999d48d518f883f3c45244032775284f208
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2025-08-08 21:54:27 +0330

    first commit

--- Cipolla/settings.py
+++ Cipolla/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ads3bn9%dmdm1et+^#m!tb&9sh52h#=^na@9*2ttg#4qj+!$7a'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = [
     'cipolla.ir',
