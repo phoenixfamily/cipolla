abc5538d5e62a0914ae686edc5f533e48b147db2
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2025-08-08 23:02:36 +0330

    first commit

--- Cipolla/urls.py
+++ Cipolla/urls.py
@@ -18,10 +18,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', include('home.urls')),  # صفحه اصلی
+    path('', RedirectView.as_view(url='home/', permanent=True)),  # ریدایرکت دائمی
+
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
