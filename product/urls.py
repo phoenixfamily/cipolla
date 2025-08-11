c444c9e873a3be743a4c5496bf9564ebbc1b0cf4
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2025-08-11 15:03:29 +0330

    first commit

--- product/urls.py
+++ product/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'product'
 
 urlpatterns = [
+    path('', views.product_view(), name='product-view'),
     path('category', views.category_view, name='category-view'),
 ]
--- product/views.py
+++ product/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def category_view(request):
     return render(request, 'category.html')
+
+
+def product_view(request):
+    return render(request, 'product.html')
